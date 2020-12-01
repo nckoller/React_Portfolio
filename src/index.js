6b526ba60679f40a1e55ad055acd4a6e4828fe41
Nina Koller <nckoller@gmail.com>
2020-11-30 22:34:09 -0600

    adding basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
     <App />
     </Router>
   </React.StrictMode>,
